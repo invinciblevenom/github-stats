5eb3b762ec027d93c15460a1ef3289f971a307f9
Ansh Singh <anshsingh.14.lko@gmail.com>
2022-02-21 19:41:10 +0530

    Update generate_images.py

--- generate_images.py
+++ generate_images.py
@@ -65,7 +65,7 @@ async def generate_languages(s: Stats) -> None:
     delay_between = 150
     for i, (lang, data) in enumerate(sorted_languages):
         color = data.get("color")
-        color = color if color is not None else "#0d1117"
+        color = color if color is not None else "#000000"
         progress += (
             f'<span style="background-color: {color};'
             f'width: {data.get("prop", 0):0.3f}%;" '
@@ -79,7 +79,6 @@ fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8z"></path></svg>
 <span class="lang">{lang}</span>
 <span class="percent">{data.get("prop", 0):0.2f}%</span>
 </li>
-
 """
 
     output = re.sub(r"{{ progress }}", progress, output)
